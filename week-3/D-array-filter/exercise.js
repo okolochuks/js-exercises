011439cea3116091ac88b7f944b84ab83d996b6b
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-03-18 15:35:41 +0000

    week-2-practise

--- week-3/D-array-filter/exercise.js
+++ week-3/D-array-filter/exercise.js
@@ -10,13 +10,13 @@ var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
 var pairsByIndex; // Complete this statement
 
+function isNonPair(arr) {
+  return arr === unpaired;
+}
+
+var arr = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
+var unpaired = arr.filter(isNonPair);
+console.log(unpaired);
+
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
-
-var pairs = pairsByIndex.map(function(indexes) {
-  var student = students[indexes[0]];
-  var mentor = mentors[indexes[1]];
-  return [student, mentor];
-});
-
-console.log(pairs);
